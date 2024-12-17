381141a107a07ec3b8e415d77c1ad44af52406c5
Will Thrussell <wt@toolkitwebsites.co.uk>
2024-12-17 12:30:12 +0000

    Add save source code scroll script

---
+++ save-source-code-scroll.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Save Source Code Scroll
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Save Scrolls
+// @author       You
+// @match        https://www.toolkit.uk/pages/source/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=toolkit.uk
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.addEventListener("load", () => {
+        setTimeout(() => {
+        document.querySelector(".CodeMirror-scroll").scrollTop = sessionStorage.getItem("scrollDown");
+
+        document.querySelector(".CodeMirror-scroll").addEventListener("scroll", (e) => {
+            sessionStorage.setItem("scrollDown", e.target.scrollTop);
+        });
+        }, 200);
+    });
+    
+})();
